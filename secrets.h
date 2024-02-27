00e59729f2ea08b8ed6e309a5a70d07b9f10e5b1
Kris Kersey <kris@kerseyfabrications.com>
2024-02-27 01:52:55 -0500

    Update secrets doc. Add json build-time link.

--- secrets.h
+++ secrets.h
@@ -1,6 +1,8 @@
 #ifndef SECRETS_H
 #define SECRETS_H
 
+/* This is the API key from OpenAI. This is an INVALID key. Change this to your key after
+ * registering for one on their site. */
 #define OPENAI_HEADER   "Authorization: Bearer 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"
 
 #endif // SECRETS_H
