650159bc55670d1719a1d6b69f51d989da78f39a
Kris Kersey <kris@kerseyfabrications.com>
2025-10-31 20:05:47 +0000

    Fix credentials and add header.

--- remote_dawn/remote_dawn.ino
+++ remote_dawn/remote_dawn.ino
@@ -1,3 +1,24 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * By contributing to this project, you agree to license your contributions
+ * under the GPLv3 (or any later version) or any future licenses chosen by
+ * the project author(s). Contributions include any modifications,
+ * enhancements, or additions to the project. These contributions become
+ * part of the project and are adopted by the project author(s).
+ */
+
 #include <WiFi.h>
 #include <driver/i2s_std.h>
 #include <driver/i2s_types.h>
@@ -8,8 +29,8 @@
 #include <SPI.h>
 
 // WiFi credentials
-const char* ssid = "TORTUGA";
-const char* password = "Large Hadron Collider";
+const char* ssid = "STARK INDUSTRIES";
+const char* password = "IronMan123";
 
 // Server settings
 const char* SERVER_IP = "192.168.1.159";
